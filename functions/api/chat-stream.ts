b1d298f524199ea1b032c677609821585e858e2c
ptp-build <127082179+ptp-build@users.noreply.github.com>
2023-04-16 17:36:52 +0800

    change functions

--- functions/api/chat-stream.ts
+++ functions/api/chat-stream.ts
@@ -1,40 +1,4 @@
-import {requestOpenai} from "./common";
-import {createParser} from "../../src/lib/eventsource-parser";
-
-async function createStream(req: Request) {
-  const encoder = new TextEncoder();
-  const decoder = new TextDecoder();
-  const res = await requestOpenai(req);
-  return new ReadableStream({
-    async start(controller) {
-      function onParse(event: any) {
-        if (event.type === "event") {
-          const data = event.data;
-          // https://beta.openai.com/docs/api-reference/completions/create#completions/create-stream
-          if (data === "[DONE]") {
-            controller.close();
-            return;
-          }
-          try {
-            const json = JSON.parse(data);
-            const text = json.choices[0].delta.content;
-            const queue = encoder.encode(text);
-            controller.enqueue(queue);
-          } catch (e) {
-            controller.error(e);
-          }
-        }
-      }
-
-      const parser = createParser(onParse);
-      for await (const chunk of res.body as any) {
-        parser.feed(decoder.decode(chunk));
-      }
-    },
-  });
-}
-
-
+import {createStream} from "../../src/worker/share/functions/openai";
 
 export const onRequestPost: PagesFunction = async ({request}) => {
   try {
