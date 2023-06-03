24e9e0d1c242bae975f029d0948f3e869461eadb
ai-fork <barry.ptp.build@gmail.com>
2023-06-03 14:17:51 +0800

    [Build]

--- src/worker/msg/MobileBridge.ts
+++ src/worker/msg/MobileBridge.ts
@@ -1,9 +1,9 @@
 import {getWebPlatform} from "../../global/actions/api/initial";
 
 export type MobileBridgeAction = "APP_INIT"
-export default class
-MobileBridge {
+export default class MobileBridge {
   static postEvent(eventName:MobileBridgeAction,eventData?:any){
+    console.log("MobileBridge postEvent",eventName,eventData,getWebPlatform())
     if(getWebPlatform() === "android"){
       // @ts-ignore
       window.WaiBridge.postEvent(eventName,payload ? JSON.stringify(eventData) : "{}")
